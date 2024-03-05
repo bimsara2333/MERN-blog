7887687761e5ee25478707ce6efd1e4263b3fe15
bimsara2333 <bimsara.s24035stc@gmail.com>
2024-03-05 16:51:24 +0530

    second

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/home';
+import SignIn from './pages/signIn';
+import Signup from './pages/signup';
+import Dashboard from './pages/dashboard';
+import About from './pages/about';
 
 export default function App() {
   return (
     <div>
-      <h1 className='text-3xl text-red-500'>Hello !!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/signin" element={<SignIn />}/>
+          <Route path="/signup" element={<Signup />}/>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
