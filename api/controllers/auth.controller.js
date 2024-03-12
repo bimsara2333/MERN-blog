1e93e78f4bc159dbdb26d6e205b6a1970daa7dc8
bimsara2333 <bimsara.s24035stc@gmail.com>
2024-03-12 12:56:20 +0530

    signup db done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,21 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+
 export const signup = async (req, res) =>{
-    console.log(req.body);
-}
+    const { username, email, password} = req.body;
+
+    if( !username || !email || !password || username ==='' || email==='' || password==="" ){
+        return res.status(400).json({message: 'All fields are required'});
+    }
+
+    const hashedpassword = bcryptjs.hashSync(password, 10);
+
+    const newuser = new User({username, email, password : hashedpassword});
+
+    try {
+        await newuser.save();
+        res.json({ message: 'Signup successfull'});
+    } catch (error) {
+        res.status(500).json({ message: error.message});
+    }
+};
